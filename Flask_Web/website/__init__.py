047e865a8d760b2cf99d6461e005db8965b1c1d3
Ean Siao <20058@my.sanctamaria.school.nz>
2025-07-25 12:05:15 +1200

    added code to initiate database and use secret key to hash password

--- Flask_Web/website/__init__.py
+++ Flask_Web/website/__init__.py
@@ -2,17 +2,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
 # Create app function
 # Returns app
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'Ze5m20kt1o'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     # Import views from views.py
     from .views import views
+    #from .auth import auth
     
     # Register blueprints
     app.register_blueprint(views, url_prefix="/")
+    #app.register_blueprint(auth, url_prefix="/")
     
+    from .models import User
+    
+    with app.app_context():
+        db.create_all()
     
     return app
