1f839ac8d1536a76daf46808d0924529da532fa7
Ean Siao <20058@my.sanctamaria.school.nz>
2025-07-28 09:57:34 +1200

    created route for users to login

--- Flask_Web/website/__init__.py
+++ Flask_Web/website/__init__.py
@@ -1,6 +1,7 @@
 # Import external libraries
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
@@ -28,5 +29,14 @@ def create_app():
     
     with app.app_context():
         db.create_all()
+    
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
--- Flask_Web/website/auth.py
+++ Flask_Web/website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, url_for, request, flash, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
-from .models import User, Note
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -31,8 +31,30 @@ def sign_up():
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:32768:8:1'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(new_user, remember=True)
             flash('Account Created!', category='success')
             
     return render_template("sign_up.html", user=current_user)
     
+# Login route
+@auth.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('You have logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect Password.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    return render_template('login.html', user=current_user)
+
+# Logout route
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('views.home'))
